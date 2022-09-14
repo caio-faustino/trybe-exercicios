2b2b7c2f8d342e7a2934c4dba6bbd5ef735ccbe3
caio-faustino <caioexfaustino.pro@gmail.com>
2022-09-14 19:22:14 -0300

    exercicios bloco 06 dia 01 requisitos 02 e 03

---
+++ modulos/fundamentos/bloco-06-html-css-forms-flxbox-responsivos/dia-01-html-css-forms/script.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+function handleSubmit(event) {
+	event.preventDefault();
+}
+
+window.onload = function () {
+	const submitBtn = document.querySelector("#submit-btn");
+	submitBtn.addEventListener("click", handleSubmit);
+};
